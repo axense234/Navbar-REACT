5072504ea6ee0d28fb4a58db094860c433631da4
axense234 <andreicomanescuonline@gmail.com>
2023-02-24 10:18:44 +0200

    Changed it so the showMenu and showDropdown work better.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,33 +9,39 @@ import Dropdown from "./Dropdown";
 
 const Navbar = ({ showMenu }) => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const showMenuFull = showMenu || window.innerWidth <= 1100;
 
   return (
     <nav className='navbar-container'>
       <div className='navbar-container__content'>
         <h1>axense234</h1>
-        <div className='navbar-container__page-links'>
-          {pageLinks.map((pageLink) => {
-            return (
-              <a href={pageLink.destination} key={pageLink.id}>
-                {pageLink.label}
-              </a>
-            );
-          })}
-        </div>
-        {!showMenu ? (
-          <div className='navbar-container__social-media'>
-            {socialMediaLinks.map((socialMediaLink) => {
-              return (
-                <a href={socialMediaLink.destination} key={socialMediaLink.id}>
-                  <img
-                    src={socialMediaLink.iconUrl}
-                    alt={socialMediaLink.destination}
-                  />
-                </a>
-              );
-            })}
-          </div>
+        {!showMenuFull ? (
+          <>
+            <div className='navbar-container__page-links'>
+              {pageLinks.map((pageLink) => {
+                return (
+                  <a href={pageLink.destination} key={pageLink.id}>
+                    {pageLink.label}
+                  </a>
+                );
+              })}
+            </div>
+            <div className='navbar-container__social-media'>
+              {socialMediaLinks.map((socialMediaLink) => {
+                return (
+                  <a
+                    href={socialMediaLink.destination}
+                    key={socialMediaLink.id}
+                  >
+                    <img
+                      src={socialMediaLink.iconUrl}
+                      alt={socialMediaLink.destination}
+                    />
+                  </a>
+                );
+              })}
+            </div>
+          </>
         ) : (
           <button
             type='button'
@@ -47,7 +53,7 @@ const Navbar = ({ showMenu }) => {
           </button>
         )}
       </div>
-      <Dropdown showDropdown={showDropdown} />
+      <Dropdown showDropdown={showDropdown && window.innerWidth <= 1100} />
     </nav>
   );
 };
